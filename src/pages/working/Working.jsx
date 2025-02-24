874d1e2ce0d62413f1bf172cb69c593510fce55a
Piazza <carlamalenapiazza@gmail.com>
2025-02-24 20:48:15 -0300

    working page

---
+++ src/pages/working/Working.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './css/working.css'; 
+
+const Working = () => {
+    return (
+      <div className="construction-container">
+          <div className="construction-content">
+              <img src="LOGO-PGOSApng.png" alt="Logo" />
+              <h1>¡Estamos trabajando en ello!</h1>
+              <p>Vuelve pronto</p>
+          </div>
+      </div>
+    );
+  };
+  
+  export default Working;
--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/home/Home";
+import Working from "../pages/working/Working";
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Working />} />
         </Routes>
         </BrowserRouter>
     );
